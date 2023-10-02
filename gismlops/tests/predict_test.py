181142f24df26913b155cef746cff6e323cd90d2
EgorovGA <egorov.ga@minc.tech>
2023-10-02 20:15:56 +0300

    fix tests - replace numpy syntax with pandas

--- gismlops/tests/predict_test.py
+++ gismlops/tests/predict_test.py
@@ -9,7 +9,7 @@ class TestPredictions(unittest.TestCase):
     def testPredict(self):
         train()
         predictions = infer()
-        correct = np.sum(predictions[:, 0] == predictions[:, 1])
-        every = predictions.shape[0]
+        correct = np.sum(predictions["target_index"] == predictions["predicted_index"])
+        every = len(predictions)
         PRECISION = 0.7
         self.assertTrue(correct > PRECISION * every)
