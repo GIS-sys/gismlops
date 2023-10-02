9a938ba0a4b340e522f842afa859c9ba291d9d88
EgorovGA <egorov.ga@minc.tech>
2023-10-02 19:32:23 +0300

    add pandas and save test results to file

--- gismlops/infer.py
+++ gismlops/infer.py
@@ -1,6 +1,9 @@
+import pandas as pd
 import torch
 from gismlops.model import NeuralNetwork
-from gismlops.utils import getDevice
+from gismlops.utils import epochTest, getDevice
+from torch import nn
+from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
@@ -18,28 +21,27 @@ def infer():
     device = getDevice()
     model = NeuralNetwork().to(device)
     model.load_state_dict(torch.load("model.pth"))
-
-    classes = [
-        "T-shirt/top",
-        "Trouser",
-        "Pullover",
-        "Dress",
-        "Coat",
-        "Sandal",
-        "Shirt",
-        "Sneaker",
-        "Bag",
-        "Ankle boot",
-    ]
-
     model.eval()
-    x, y = test_data[0][0], test_data[0][1]
-    with torch.no_grad():
-        x = x.to(device)
-        pred = model(x)
-        predicted, actual = classes[pred[0].argmax(0)], classes[y]
-        print(f"Predicted: {predicted}, Actual: {actual}")
-        return predicted, actual
+
+    # classes = [
+    #     "T-shirt/top",
+    #     "Trouser",
+    #     "Pullover",
+    #     "Dress",
+    #     "Coat",
+    #     "Sandal",
+    #     "Shirt",
+    #     "Sneaker",
+    #     "Bag",
+    #     "Ankle boot",
+    # ]
+
+    batch_size = 64
+    test_dataloader = DataLoader(test_data, batch_size=batch_size)
+    loss_fn = nn.CrossEntropyLoss()
+    answers = epochTest(test_dataloader, model, loss_fn, device)
+
+    pd.DataFrame(answers).to_csv("testfile.csv", index=False)
 
 
 if __name__ == "__main__":
--- gismlops/utils.py
+++ gismlops/utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 from tqdm import tqdm
 
@@ -23,6 +24,7 @@ def epochTrain(dataloader, model, loss_fn, optimizer, device, verbose_interval=1
 
 
 def epochTest(dataloader, model, loss_fn, device):
+    answers = None  # np.empty(shape=[2,0])
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
     model.eval()
@@ -33,11 +35,17 @@ def epochTest(dataloader, model, loss_fn, device):
             pred = model(X)
             test_loss += loss_fn(pred, y).item()
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+            newAns = np.stack((y, pred.argmax(1)))
+            if answers is None:
+                answers = newAns
+            else:
+                answers = np.concatenate((answers, newAns), axis=1)
     test_loss /= num_batches
     correct /= size
     print(
         f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n"
     )
+    return answers.T
 
 
 def getDevice():
